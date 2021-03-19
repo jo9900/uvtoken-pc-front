ce62c84d99001ffd13cb4107bb7bde34b95e7e05
郑媛 <zhengyuan@vbhledger.com>
2021-03-19 09:17:25 +0800

    增加 身份证校验

--- src/views/Center/KYCapply.vue
+++ src/views/Center/KYCapply.vue
@@ -182,6 +182,15 @@ export default {
         callback();
       }
     };
+    var IDValidator = (rule, value, callback) => {
+      if (this.formLabelAlign.id_type == 1 && this.formLabelAlign.country_code == 'CHN') { // 身份证号码
+        var idcardReg = /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/;
+        if (idcardReg.test(this.formLabelAlign.id_number)) callback();
+        else callback(new Error(this.$t("usercenter.text31")));
+      } else {
+        callback();
+      }
+    };
     return {
       BaseUrl: this.$BaseUrl,
       areaList: [],
@@ -236,16 +245,18 @@ export default {
           {
             required: true,
             message: this.$t( 'text137' ),
-            trigger: "blur",
+            trigger: "blur"
           },
+          { required: true, trigger: "blur", validator: IDValidator },
         ],
         isImg: [
           {
             required: true,
             message: this.$t( 'text137' ),
-            trigger: "blur",
-            validator: imgValidator,
+            trigger: "blur"
           },
+          { required: true, trigger: "blur", validator: imgValidator },
+
         ],
         birth_date: [
           {
@@ -265,7 +276,18 @@ export default {
     };
   },
   computed: {},
-  watch: {},
+  watch: {
+    'formLabelAlign.id_type' : {
+      handler(cur) {
+        if (this.formLabelAlign.country_code == 'CHN') {
+          if (cur != 1)
+            this.$refs["formLabelAlign"].clearValidate('id_number')
+          else
+            this.$refs["formLabelAlign"].validateField('id_number')
+        }
+      }
+    }
+  },
   methods: {
     subFromData() {
       this.$refs["formLabelAlign"].validate((valid) => {
