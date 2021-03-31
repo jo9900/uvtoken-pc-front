be7c8e59285df9870cb36cad4759a020e9d66145
郑媛 <zhengyuan@vbhledger.com>
2021-03-31 17:30:53 +0800

    测试用apk 二维码

--- src/views/Index/index.vue
+++ src/views/Index/index.vue
@@ -59,8 +59,8 @@
                   </div>
                   <el-dropdown-menu slot="dropdown">
                     <div class="qrcode-img">
-                      <img v-if="NODE_ENV !== 'production'" src="@/assets/img/dowemoey.png" />
-                      <img v-else src="@/assets/img/apkfortest.png" />
+                      <img v-if="NODE_ENV !== 'production'" src="@/assets/img/apkfortest.png" />apkfortest
+                      <img v-else src="@/assets/img/dowemoey.png" />
                     </div>
                   </el-dropdown-menu>
                 </el-dropdown>
