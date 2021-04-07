9afe3bf0e3de67cdb7c064267e4cbaa7aec16701
郑媛 <zhengyuan@vbhledger.com>
2021-04-07 10:31:41 +0800

    替换 图片

--- vue.config.js
+++ vue.config.js
@@ -94,9 +94,9 @@ module.exports = {
             .use('image-webpack-loader')
             .loader('image-webpack-loader')
             .options({
-                mozjpeg: { progressive: true, quality: 50 },
+                mozjpeg: { progressive: true, quality: 65 },
                 optipng: { enabled: false },
-                pngquant: { quality: [0.5, 0.6], speed: 4 },
+                pngquant: { quality: [0.65, 0.9], speed: 4 },
                 gifsicle: { interlaced: false }
             })
     }
